4788573072aebebc3f68808c7bf48e507736a14a
josieruiter <josieruiter@35.40.61.30>
2018-11-28 18:36:46 -0500

    rtc- also making sure this works

--- 226 Final Project/main.c
+++ 226 Final Project/main.c
@@ -26,6 +26,11 @@ void pushByte(uint8_t byte);    //pushes most significant 4 bits to data pins wi
 void write_command( uint8_t command); //writes one bit of command by calling pushByte() with the command parameter
 void dataWrite(uint8_t data);   //will write one bit of data by calling pushByte()
 
+void RTC_Init();
+
+int time_update = 0, alarm_update = 0;
+uint8_t hours, mins, secs;
+
 static volatile uint16_t result; //vars used in temp reading
 float nADC;
 char line2[50];
@@ -34,6 +39,8 @@ void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
     systick_start(); //Start up Systick
+    RTC_Init();
+    __enable_interrupt();
 
     LCD_init(); //LCD initialization
     ADC14_init();
@@ -215,4 +222,46 @@ void readtemp()
     nADC= (nADC-500)/10;
     sprintf(line2, "%.1f", nADC);
 
+}
+//---------------------------------------------------------------------------------------------------
+void RTC_Init(){
+    //Initialize time to 2:45:55 pm
+//    RTC_C->TIM0 = 0x2D00;  //45 min, 0 secs
+    RTC_C->CTL0 = (0xA500);
+    RTC_C->CTL13 = 0;
+
+    RTC_C->TIM0 = 45<<8 | 55;//45 min, 55 secs
+    RTC_C->TIM1 = 1<<8 | 14;  //Monday, 2 pm
+    RTC_C->YEAR = 2018;
+    //Alarm at 2:46 pm
+    RTC_C->AMINHR = 14<<8 | 46 | BIT(15) | BIT(7);  //bit 15 and 7 are Alarm Enable bits
+    RTC_C->ADOWDAY = 0;
+    RTC_C->PS1CTL = 0b00010;  //1/64 second interrupt
+
+    RTC_C->CTL0 = (0xA500) | BIT5; //turn on interrupt
+    RTC_C->CTL13 = 0;
+    //TODO
+    NVIC_EnableIRQ(RTC_C_IRQn);
+}
+//---------------------------------------------------------------------------------------------------
+void RTC_C_IRQHandler()
+{
+    if(RTC_C->PS1CTL & BIT0){
+        hours = RTC_C->TIM1 & 0x00FF;
+        mins = (RTC_C->TIM0 & 0xFF00) >> 8;
+        secs = RTC_C->TIM0 & 0x00FF;
+        if(secs != 59){
+            RTC_C->TIM0 = RTC_C->TIM0 + 1;
+        }
+        else {
+            RTC_C->TIM0 = (((RTC_C->TIM0 & 0xFF00) >> 8)+1)<<8;
+            time_update = 1;
+        }
+        RTC_C->PS1CTL &= ~BIT0;
+    }
+    if(RTC_C->CTL0 & BIT1)
+    {
+        alarm_update = 1;
+        RTC_C->CTL0 = (0xA500) | BIT5;
+    }
 }
